4582a8b7385ab3dc857d9b0e5ea2e8fc7d7e83a6
giih97 <gbamdev9@gmail.com>
2021-04-09 17:44:13 -0300

    add o construtor

--- src/main/java/br/com/dio/model/Evento.java
+++ src/main/java/br/com/dio/model/Evento.java
@@ -4,15 +4,27 @@ import java.io.Serializable;
 
 public class Evento implements Serializable {
 	
-	
-
 	private static final long serialVersionUID = 1L;
 	
 	private String nome;
-	private String organização;
+	private String organizacao;
 	private String local;
 	private String data;
 	private String horario;
+	
+	public Evento() {
+		
+	}
+	
+	public Evento (String nome, String organizacao, String local, String data, String hora) {
+		super();
+		this.nome = nome;
+		this.organizacao = organizacao;
+		this.local = local;
+		this.data = data;
+		this.horario = horario;
+	}
+	
 	
 	public String getNome() {
 		return nome;
@@ -20,11 +32,11 @@ public class Evento implements Serializable {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	public String getOrganização() {
-		return organização;
+	public String getOrganizacao() {
+		return organizacao;
 	}
-	public void setOrganização(String organização) {
-		this.organização = organização;
+	public void setOrganizacao(String organizacao) {
+		this.organizacao = organizacao;
 	}
 	public String getLocal() {
 		return local;
