a6314d2b08a3142ea4811fa78927a798ec25114f
giih97 <gbamdev9@gmail.com>
2021-04-09 16:38:57 -0300

    add injeção de dependencia na classe EventoBean

--- src/main/java/br/com/dio/controller/EventoBean.java
+++ src/main/java/br/com/dio/controller/EventoBean.java
@@ -2,6 +2,16 @@ package br.com.dio.controller;
 
 import java.io.Serializable;
 
-public class EventoBean implements Serializable {
+import javax.inject.Inject;
+import javax.inject.Named;
 
+import br.com.dio.model.Evento;
+
+@Named
+public class EventoBean implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private Evento evento;
 }
