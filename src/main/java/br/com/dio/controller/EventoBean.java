4d0609123cb3375d102262d0539dc1b4897963ef
giih97 <gbamdev9@gmail.com>
2021-04-09 17:43:32 -0300

    add metodo clean

--- src/main/java/br/com/dio/controller/EventoBean.java
+++ src/main/java/br/com/dio/controller/EventoBean.java
@@ -1,7 +1,9 @@
 package br.com.dio.controller;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
+import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
@@ -10,6 +12,7 @@ import com.sun.tools.javac.util.List;
 import br.com.dio.model.Evento;
 
 @Named
+@SessionScoped
 public class EventoBean implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
@@ -17,11 +20,17 @@ public class EventoBean implements Serializable {
 	@Inject
 	private Evento evento;
 	
-	private List<Evento> eventos = new ArrayList<Evento>();
+	private ArrayList<Evento> eventos = new ArrayList<Evento>();
 	
-	public String adicionarEvento() {
+	public String adicionarEvento() { // metodo
 		eventos.add(evento);
 		System.out.println("Evento" + evento.getNome() + "cadastrado com sucesso");
+		clean();
+		return "";
+	}
+	
+	public void clean() {
+		this.evento = new Evento();
 	}
 
 	public Evento getEvento() {
