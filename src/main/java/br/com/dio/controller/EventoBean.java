3d7a4c10358151d48d23de59dbc06ab4819148dc
giih97 <gbamdev9@gmail.com>
2021-04-09 16:48:35 -0300

    add metodod para imprimir na tela uma mensagem

--- src/main/java/br/com/dio/controller/EventoBean.java
+++ src/main/java/br/com/dio/controller/EventoBean.java
@@ -5,6 +5,8 @@ import java.io.Serializable;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import com.sun.tools.javac.util.List;
+
 import br.com.dio.model.Evento;
 
 @Named
@@ -14,4 +16,29 @@ public class EventoBean implements Serializable {
 
 	@Inject
 	private Evento evento;
+	
+	private List<Evento> eventos = new ArrayList<Evento>();
+	
+	public String adicionarEvento() {
+		eventos.add(evento);
+		System.out.println("Evento" + evento.getNome() + "cadastrado com sucesso");
+	}
+
+	public Evento getEvento() {
+		return evento;
+	}
+
+	public void setEvento(Evento evento) {
+		this.evento = evento;
+	}
+
+	public List<Evento> getEventos() {
+		return eventos;
+	}
+
+	public void setEventos(List<Evento> eventos) {
+		this.eventos = eventos;
+	}
+	
+	
 }
--- src/main/java/br/com/dio/model/Evento.java
+++ src/main/java/br/com/dio/model/Evento.java
@@ -3,7 +3,47 @@ package br.com.dio.model;
 import java.io.Serializable;
 
 public class Evento implements Serializable {
+	
+	
 
 	private static final long serialVersionUID = 1L;
-
+	
+	private String nome;
+	private String organização;
+	private String local;
+	private String data;
+	private String horario;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getOrganização() {
+		return organização;
+	}
+	public void setOrganização(String organização) {
+		this.organização = organização;
+	}
+	public String getLocal() {
+		return local;
+	}
+	public void setLocal(String local) {
+		this.local = local;
+	}
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+	public String getHorario() {
+		return horario;
+	}
+	public void setHorario(String horario) {
+		this.horario = horario;
+	}
+	
+	
 }
